5deaa2a3c6c44863629781ef026c4adaa27d38c5
Mushrif Ali Khan <mushrifali@gmail.com>
2025-05-06 23:24:16 +0530

    removed __dirname

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const firebaseExport = require("../firebase-admin/export");
 // Path to your service account key JSON file
 firebaseExport.initFirestore('../accountancy-app-production-firebase-adminsdk-g22ib-316ed760c0')
 
-firebaseExport.exportDataPerUIDAndYear(__dirname).catch(console.error);
+firebaseExport.exportDataPerUIDAndYear().catch(console.error);
